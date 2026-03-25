c2493555d033bfcb88f68276bdc90e10194b1cd1
ayushraj706 <ayushrajayushhh@gmail.com>
2026-03-26 01:10:43 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,12 +1,29 @@
 package com.ayush.ytpro;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    
     @Override
-    public void onPause() { // Yahan 'public' hona zaroori hai
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Ye code links ko bahar jaane se rokega
+        WebView webView = this.bridge.getWebView();
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // Har link ko app ke andar hi load karo
+                view.loadUrl(url);
+                return true; 
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
         super.onPause();
         if (this.bridge != null && this.bridge.getWebView() != null) {
             this.bridge.getWebView().resumeTimers();
@@ -14,7 +31,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    public void onStop() { // Yahan bhi 'public' kar diya
+    public void onStop() {
         super.onStop();
         if (this.bridge != null && this.bridge.getWebView() != null) {
             this.bridge.getWebView().resumeTimers();
